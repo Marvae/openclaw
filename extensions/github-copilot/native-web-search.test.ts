Anas329796/Anastasios

feat(github-copilot): add native web search for Copilot GPT models
Copilot's GPT models use the OpenAI Responses API and support the same
native web_search tool. This adds an independent native web search
implementation in the github-copilot plugin, following the same pattern
as extensions/openai/native-web-search.ts but self-contained within the
Copilot plugin — no cross-plugin dependencies.

Changes:
1. extensions/github-copilot/native-web-search.ts: New file implementing
   Copilot-specific native web search payload patching. Only activates
   for models with api=openai-responses and provider=github-copilot.
2. extensions/github-copilot/stream.ts: Wrap the provider stream with
   createCopilotNativeWebSearchWrapper.

Behavior:
- When web search is enabled and the model is a Copilot GPT model
  (openai-responses API), the managed web_search function tool is
  replaced with the native {type: web_search} tool.
- The model performs server-side web search and returns results with
  url_citation annotations — no external search API key needed.
- Claude/Gemini models on Copilot are unaffected (different API types).
- If a managed search provider is explicitly pinned, native search
  is not injected.

Verified via direct Copilot API testing (curl) and OpenClaw e2e
(subagent with github-copilot/gpt-5.4).

extensions/github-copilot/native-web-search.test.ts
@@ -1,0 +1,193 @@
+import { describe, expect, it } from "vitest";
+import {
+  createCopilotNativeWebSearchWrapper,
+  patchCopilotNativeWebSearchPayload,
+} from "./native-web-search.js";
+
+describe("patchCopilotNativeWebSearchPayload", () => {
+  it("returns payload_not_object for non-object input", () => {
+    expect(patchCopilotNativeWebSearchPayload(null)).toBe("payload_not_object");
+    expect(patchCopilotNativeWebSearchPayload(undefined)).toBe("payload_not_object");
+    expect(patchCopilotNativeWebSearchPayload("string")).toBe("payload_not_object");
+    expect(patchCopilotNativeWebSearchPayload(42)).toBe("payload_not_object");
+    expect(patchCopilotNativeWebSearchPayload([1, 2])).toBe("payload_not_object");
+  });
+
+  it("injects web_search tool into empty tools array", () => {
+    const payload: Record<string, unknown> = { tools: [] };
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("injected");
+    expect(payload.tools).toEqual([{ type: "web_search" }]);
+  });
+
+  it("injects web_search tool when tools is undefined", () => {
+    const payload: Record<string, unknown> = {};
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("injected");
+    expect(payload.tools).toEqual([{ type: "web_search" }]);
+  });
+
+  it("replaces managed web_search function tool with native tool", () => {
+    const payload: Record<string, unknown> = {
+      tools: [
+        { type: "function", name: "web_search" },
+        { type: "function", name: "other_tool" },
+      ],
+    };
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("injected");
+    expect(payload.tools).toEqual([
+      { type: "function", name: "other_tool" },
+      { type: "web_search" },
+    ]);
+  });
+
+  it("returns native_tool_already_present when native tool exists", () => {
+    const payload: Record<string, unknown> = {
+      tools: [{ type: "web_search" }, { type: "function", name: "other" }],
+    };
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("native_tool_already_present");
+    // should not duplicate
+    expect(payload.tools).toEqual([{ type: "web_search" }, { type: "function", name: "other" }]);
+  });
+
+  it("removes managed tool when native tool already present", () => {
+    const payload: Record<string, unknown> = {
+      tools: [
+        { type: "web_search" },
+        { type: "function", name: "web_search" },
+        { type: "function", name: "other" },
+      ],
+    };
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("native_tool_already_present");
+    expect(payload.tools).toEqual([{ type: "web_search" }, { type: "function", name: "other" }]);
+  });
+
+  it("raises minimal reasoning effort to low", () => {
+    const payload: Record<string, unknown> = {
+      tools: [],
+      reasoning: { effort: "minimal" },
+    };
+    patchCopilotNativeWebSearchPayload(payload);
+    expect((payload.reasoning as Record<string, unknown>).effort).toBe("low");
+  });
+
+  it("does not change non-minimal reasoning effort", () => {
+    const payload: Record<string, unknown> = {
+      tools: [],
+      reasoning: { effort: "medium" },
+    };
+    patchCopilotNativeWebSearchPayload(payload);
+    expect((payload.reasoning as Record<string, unknown>).effort).toBe("medium");
+  });
+
+  it("does not change reasoning when it is not an object", () => {
+    const payload: Record<string, unknown> = {
+      tools: [],
+      reasoning: "high",
+    };
+    patchCopilotNativeWebSearchPayload(payload);
+    expect(payload.reasoning).toBe("high");
+  });
+});
+
+describe("createCopilotNativeWebSearchWrapper", () => {
+  const mockStreamFn = (() => {
+    const fn = (_model: unknown, _context: unknown, _options: unknown) => {
+      return { __mock: true };
+    };
+    return fn;
+  })() as any;
+
+  it("skips non-eligible models (wrong api)", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "chat-completions", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips non-eligible models (wrong provider)", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "openai-responses", provider: "openai", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips Gemini models on Copilot (not OpenAI-family)", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gemini-2.5-pro" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips unknown model ids on Copilot", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "goldeneye" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips when web search is disabled in config", () => {
+    const config = { tools: { web: { search: { enabled: false } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips when web search provider is pinned to non-openai", () => {
+    const config = { tools: { web: { search: { provider: "brave" } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("activates for eligible model with auto provider", () => {
+    const config = { tools: { web: { search: { provider: "auto" } } } } as any;
+    let payloadPatched = false;
+    const trackingStreamFn = (model: any, context: any, options: any) => {
+      // Check if payload was patched (tools array modified)
+      if (options?.body) {
+        const body = JSON.parse(options.body);
+        if (body.tools?.some((t: any) => t.type === "web_search")) payloadPatched = true;
+      }
+      return { __mock: true };
+    };
+    const wrapper = createCopilotNativeWebSearchWrapper(trackingStreamFn as any, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    // The wrapper should not skip — it should go through streamWithPayloadPatch
+    // We verify by checking it doesn't directly return (it wraps the call)
+    const result = wrapper(model as any, {} as any, {} as any);
+    // streamWithPayloadPatch returns the underlying streamFn result
+    // The key is that it didn't bail early (skip conditions)
+    expect(result).toBeDefined();
+  });
+
+  it("activates for eligible model with empty provider", () => {
+    const config = { tools: { web: { search: { provider: "" } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    // Should not bail early
+    expect(result).toBeDefined();
+  });
+
+  it("activates for eligible model with openai provider", () => {
+    const config = { tools: { web: { search: { provider: "openai" } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toBeDefined();
+  });
+
+  it("passes through for non-eligible model", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "chat-completions", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    // Non-eligible → passes through directly
+    expect(result).toEqual({ __mock: true });
+  });
+});

extensions/github-copilot/native-web-search.ts
@@ -1,0 +1,128 @@
+import type { StreamFn } from "@mariozechner/pi-agent-core";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/config-types";
+import { streamWithPayloadPatch } from "openclaw/plugin-sdk/provider-stream-shared";
+
+/**
+ * Native web search for GitHub Copilot GPT models.
+ *
+ * Copilot's GPT models use the OpenAI Responses API and support the same
+ * native `web_search` tool as OpenAI direct. When enabled, this injects the
+ * tool into the request payload so the model can perform server-side web
+ * searches without a separate managed search provider or API key.
+ *
+ * Only activates for models using api: "openai-responses" on the
+ * github-copilot provider. Claude and Gemini models on Copilot use different
+ * APIs and are unaffected.
+ */
+
+const COPILOT_WEB_SEARCH_TOOL = { type: "web_search" } as const;
+
+function isRecord(value: unknown): value is Record<string, unknown> {
+  return !!value && typeof value === "object" && !Array.isArray(value);
+}
+
+/**
+ * Known model ID prefixes that support native web search on Copilot.
+ * Only OpenAI-family models (GPT, o-series) accept the native {type: "web_search"} tool.
+ * Gemini and other non-OpenAI models routed through Copilot's openai-responses API
+ * do NOT support this tool shape.
+ */
+const COPILOT_NATIVE_WEB_SEARCH_MODEL_PREFIXES = ["gpt-", "o1-", "o3-", "o4-", "chatgpt-"];
+
+function isCopilotNativeWebSearchEligibleModel(model: {
+  api?: unknown;
+  provider?: unknown;
+  id?: unknown;
+}): boolean {
+  if (model.api !== "openai-responses") {
+    return false;
+  }
+  const provider = typeof model.provider === "string" ? model.provider : undefined;
+  if (provider !== "github-copilot") {
+    return false;
+  }
+  const modelId = typeof model.id === "string" ? model.id.toLowerCase() : "";
+  return COPILOT_NATIVE_WEB_SEARCH_MODEL_PREFIXES.some((prefix) => modelId.startsWith(prefix));
+}
+
+/**
+ * Check if the configured web search provider is compatible with native web search.
+ * "openai" here means the OpenAI Responses API native web_search tool shape,
+ * including Copilot GPT models that proxy Responses-compatible payloads.
+ */
+function shouldUseNativeWebSearchProvider(config: OpenClawConfig | undefined): boolean {
+  const provider = config?.tools?.web?.search?.provider;
+  if (typeof provider !== "string") {
+    return true;
+  }
+  const normalized = provider.trim().toLowerCase();
+  return normalized === "" || normalized === "auto" || normalized === "openai";
+}
+
+function shouldEnableCopilotNativeWebSearch(params: {
+  config?: OpenClawConfig;
+  model: { api?: unknown; provider?: unknown; id?: unknown };
+}): boolean {
+  return (
+    params.config?.tools?.web?.search?.enabled !== false &&
+    shouldUseNativeWebSearchProvider(params.config) &&
+    isCopilotNativeWebSearchEligibleModel(params.model)
+  );
+}
+
+function isNativeWebSearchTool(tool: unknown): boolean {
+  return isRecord(tool) && tool.type === COPILOT_WEB_SEARCH_TOOL.type;
+}
+
+function isManagedWebSearchTool(tool: unknown): boolean {
+  return isRecord(tool) && tool.type === "function" && tool.name === COPILOT_WEB_SEARCH_TOOL.type;
+}
+
+function raiseMinimalReasoningForNativeWebSearch(payload: Record<string, unknown>): void {
+  const reasoning = payload.reasoning;
+  if (!isRecord(reasoning) || reasoning.effort !== "minimal") {
+    return;
+  }
+  reasoning.effort = "low";
+}
+
+export type CopilotNativeWebSearchPatchResult =
+  | "payload_not_object"
+  | "native_tool_already_present"
+  | "injected";
+
+export function patchCopilotNativeWebSearchPayload(
+  payload: unknown,
+): CopilotNativeWebSearchPatchResult {
+  if (!isRecord(payload)) {
+    return "payload_not_object";
+  }
+
+  const existingTools = Array.isArray(payload.tools) ? payload.tools : [];
+  const filteredTools = existingTools.filter((tool) => !isManagedWebSearchTool(tool));
+  if (filteredTools.some(isNativeWebSearchTool)) {
+    if (filteredTools.length !== existingTools.length) {
+      payload.tools = filteredTools;
+    }
+    raiseMinimalReasoningForNativeWebSearch(payload);
+    return "native_tool_already_present";
+  }
+
+  payload.tools = [...filteredTools, COPILOT_WEB_SEARCH_TOOL];
+  raiseMinimalReasoningForNativeWebSearch(payload);
+  return "injected";
+}
+
+export function createCopilotNativeWebSearchWrapper(
+  baseStreamFn: StreamFn,
+  params: { config?: OpenClawConfig },
+): StreamFn {
+  return (model, context, options) => {
+    if (!shouldEnableCopilotNativeWebSearch({ config: params.config, model })) {
+      return baseStreamFn(model, context, options);
+    }
+    return streamWithPayloadPatch(baseStreamFn, model, context, options, (payload) => {
+      patchCopilotNativeWebSearchPayload(payload);
+    });
+  };
+}

Marvae/openclaw

extensions/github-copilot/native-web-search.test.ts
@@ -1,0 +1,196 @@
+import { describe, expect, it } from "vitest";
+import {
+  createCopilotNativeWebSearchWrapper,
+  patchCopilotNativeWebSearchPayload,
+} from "./native-web-search.js";
+
+describe("patchCopilotNativeWebSearchPayload", () => {
+  it("returns payload_not_object for non-object input", () => {
+    expect(patchCopilotNativeWebSearchPayload(null)).toBe("payload_not_object");
+    expect(patchCopilotNativeWebSearchPayload(undefined)).toBe("payload_not_object");
+    expect(patchCopilotNativeWebSearchPayload("string")).toBe("payload_not_object");
+    expect(patchCopilotNativeWebSearchPayload(42)).toBe("payload_not_object");
+    expect(patchCopilotNativeWebSearchPayload([1, 2])).toBe("payload_not_object");
+  });
+
+  it("injects web_search tool into empty tools array", () => {
+    const payload: Record<string, unknown> = { tools: [] };
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("injected");
+    expect(payload.tools).toEqual([{ type: "web_search" }]);
+  });
+
+  it("injects web_search tool when tools is undefined", () => {
+    const payload: Record<string, unknown> = {};
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("injected");
+    expect(payload.tools).toEqual([{ type: "web_search" }]);
+  });
+
+  it("replaces managed web_search function tool with native tool", () => {
+    const payload: Record<string, unknown> = {
+      tools: [
+        { type: "function", name: "web_search" },
+        { type: "function", name: "other_tool" },
+      ],
+    };
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("injected");
+    expect(payload.tools).toEqual([
+      { type: "function", name: "other_tool" },
+      { type: "web_search" },
+    ]);
+  });
+
+  it("returns native_tool_already_present when native tool exists", () => {
+    const payload: Record<string, unknown> = {
+      tools: [{ type: "web_search" }, { type: "function", name: "other" }],
+    };
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("native_tool_already_present");
+    // should not duplicate
+    expect(payload.tools).toEqual([{ type: "web_search" }, { type: "function", name: "other" }]);
+  });
+
+  it("removes managed tool when native tool already present", () => {
+    const payload: Record<string, unknown> = {
+      tools: [
+        { type: "web_search" },
+        { type: "function", name: "web_search" },
+        { type: "function", name: "other" },
+      ],
+    };
+    const result = patchCopilotNativeWebSearchPayload(payload);
+    expect(result).toBe("native_tool_already_present");
+    expect(payload.tools).toEqual([{ type: "web_search" }, { type: "function", name: "other" }]);
+  });
+
+  it("raises minimal reasoning effort to low", () => {
+    const payload: Record<string, unknown> = {
+      tools: [],
+      reasoning: { effort: "minimal" },
+    };
+    patchCopilotNativeWebSearchPayload(payload);
+    expect((payload.reasoning as Record<string, unknown>).effort).toBe("low");
+  });
+
+  it("does not change non-minimal reasoning effort", () => {
+    const payload: Record<string, unknown> = {
+      tools: [],
+      reasoning: { effort: "medium" },
+    };
+    patchCopilotNativeWebSearchPayload(payload);
+    expect((payload.reasoning as Record<string, unknown>).effort).toBe("medium");
+  });
+
+  it("does not change reasoning when it is not an object", () => {
+    const payload: Record<string, unknown> = {
+      tools: [],
+      reasoning: "high",
+    };
+    patchCopilotNativeWebSearchPayload(payload);
+    expect(payload.reasoning).toBe("high");
+  });
+});
+
+describe("createCopilotNativeWebSearchWrapper", () => {
+  const mockStreamFn = (() => {
+    const fn = (_model: unknown, _context: unknown, _options: unknown) => {
+      return { __mock: true };
+    };
+    return fn;
+  })() as any;
+
+  it("skips non-eligible models (wrong api)", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "chat-completions", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips non-eligible models (wrong provider)", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "openai-responses", provider: "openai", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips Gemini models on Copilot (not OpenAI-family)", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gemini-2.5-pro" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips unknown model ids on Copilot", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "goldeneye" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips when web search is disabled in config", () => {
+    const config = { tools: { web: { search: { enabled: false } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("skips when web search provider is pinned to non-openai", () => {
+    const config = { tools: { web: { search: { provider: "brave" } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toEqual({ __mock: true });
+  });
+
+  it("activates for eligible model with auto provider", () => {
+    const config = { tools: { web: { search: { provider: "auto" } } } } as any;
+    const trackingStreamFn = (model: any, _context: any, options: any) => {
+      // Verify the wrapper passes through to streamWithPayloadPatch
+      return { __mock: true };
+    };
+    const wrapper = createCopilotNativeWebSearchWrapper(trackingStreamFn as any, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    // The wrapper should not skip — it should go through streamWithPayloadPatch
+    // We verify by checking it doesn't directly return (it wraps the call)
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toBeDefined();
+  });
+
+  it("activates for exact o-series model IDs (o1, o3, o4)", () => {
+    const config = { tools: { web: { search: { provider: "auto" } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    for (const id of ["o1", "o3", "o4"]) {
+      const model = { api: "openai-responses", provider: "github-copilot", id };
+      const result = wrapper(model as any, {} as any, {} as any);
+      expect(result).toBeDefined();
+    }
+  });
+
+  it("activates for eligible model with empty provider", () => {
+    const config = { tools: { web: { search: { provider: "" } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    // Should not bail early
+    expect(result).toBeDefined();
+  });
+
+  it("activates for eligible model with openai provider", () => {
+    const config = { tools: { web: { search: { provider: "openai" } } } } as any;
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config });
+    const model = { api: "openai-responses", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    expect(result).toBeDefined();
+  });
+
+  it("passes through for non-eligible model", () => {
+    const wrapper = createCopilotNativeWebSearchWrapper(mockStreamFn, { config: undefined });
+    const model = { api: "chat-completions", provider: "github-copilot", id: "gpt-5.4" };
+    const result = wrapper(model as any, {} as any, {} as any);
+    // Non-eligible → passes through directly
+    expect(result).toEqual({ __mock: true });
+  });
+});

extensions/github-copilot/native-web-search.ts
@@ -1,0 +1,132 @@
+import type { StreamFn } from "@earendil-works/pi-agent-core";
+import type { OpenClawConfig } from "openclaw/plugin-sdk/config-contracts";
+import { streamWithPayloadPatch } from "openclaw/plugin-sdk/provider-stream-shared";
+
+/**
+ * Native web search for GitHub Copilot GPT models.
+ *
+ * Copilot's GPT models use the OpenAI Responses API and support the same
+ * native `web_search` tool as OpenAI direct. When enabled, this injects the
+ * tool into the request payload so the model can perform server-side web
+ * searches without a separate managed search provider or API key.
+ *
+ * Only activates for models using api: "openai-responses" on the
+ * github-copilot provider. Claude and Gemini models on Copilot use different
+ * APIs and are unaffected.
+ */
+
+const COPILOT_WEB_SEARCH_TOOL = { type: "web_search" } as const;
+
+function isRecord(value: unknown): value is Record<string, unknown> {
+  return !!value && typeof value === "object" && !Array.isArray(value);
+}
+
+/**
+ * Known model ID prefixes that support native web search on Copilot.
+ * Only OpenAI-family models (GPT, o-series) accept the native {type: "web_search"} tool.
+ * Gemini and other non-OpenAI models routed through Copilot's openai-responses API
+ * do NOT support this tool shape.
+ */
+const COPILOT_NATIVE_WEB_SEARCH_MODEL_PREFIXES = ["gpt-", "o1-", "o3-", "o4-", "chatgpt-"];
+const COPILOT_NATIVE_WEB_SEARCH_EXACT_MODELS = new Set(["o1", "o3", "o4"]);
+
+function isCopilotNativeWebSearchEligibleModel(model: {
+  api?: unknown;
+  provider?: unknown;
+  id?: unknown;
+}): boolean {
+  if (model.api !== "openai-responses") {
+    return false;
+  }
+  const provider = typeof model.provider === "string" ? model.provider : undefined;
+  if (provider !== "github-copilot") {
+    return false;
+  }
+  const modelId = typeof model.id === "string" ? model.id.toLowerCase() : "";
+  return (
+    COPILOT_NATIVE_WEB_SEARCH_EXACT_MODELS.has(modelId) ||
+    COPILOT_NATIVE_WEB_SEARCH_MODEL_PREFIXES.some((prefix) => modelId.startsWith(prefix))
+  );
+}
+
+/**
+ * Check if the configured web search provider is compatible with native web search.
+ * "openai" here means the OpenAI Responses API native web_search tool shape,
+ * including Copilot GPT models that proxy Responses-compatible payloads.
+ */
+function shouldUseNativeWebSearchProvider(config: OpenClawConfig | undefined): boolean {
+  const provider = config?.tools?.web?.search?.provider;
+  if (typeof provider !== "string") {
+    return true;
+  }
+  const normalized = provider.trim().toLowerCase();
+  return normalized === "" || normalized === "auto" || normalized === "openai";
+}
+
+function shouldEnableCopilotNativeWebSearch(params: {
+  config?: OpenClawConfig;
+  model: { api?: unknown; provider?: unknown; id?: unknown };
+}): boolean {
+  return (
+    params.config?.tools?.web?.search?.enabled !== false &&
+    shouldUseNativeWebSearchProvider(params.config) &&
+    isCopilotNativeWebSearchEligibleModel(params.model)
+  );
+}
+
+function isNativeWebSearchTool(tool: unknown): boolean {
+  return isRecord(tool) && tool.type === COPILOT_WEB_SEARCH_TOOL.type;
+}
+
+function isManagedWebSearchTool(tool: unknown): boolean {
+  return isRecord(tool) && tool.type === "function" && tool.name === COPILOT_WEB_SEARCH_TOOL.type;
+}
+
+function raiseMinimalReasoningForNativeWebSearch(payload: Record<string, unknown>): void {
+  const reasoning = payload.reasoning;
+  if (!isRecord(reasoning) || reasoning.effort !== "minimal") {
+    return;
+  }
+  reasoning.effort = "low";
+}
+
+export type CopilotNativeWebSearchPatchResult =
+  | "payload_not_object"
+  | "native_tool_already_present"
+  | "injected";
+
+export function patchCopilotNativeWebSearchPayload(
+  payload: unknown,
+): CopilotNativeWebSearchPatchResult {
+  if (!isRecord(payload)) {
+    return "payload_not_object";
+  }
+
+  const existingTools = Array.isArray(payload.tools) ? payload.tools : [];
+  const filteredTools = existingTools.filter((tool) => !isManagedWebSearchTool(tool));
+  if (filteredTools.some(isNativeWebSearchTool)) {
+    if (filteredTools.length !== existingTools.length) {
+      payload.tools = filteredTools;
+    }
+    raiseMinimalReasoningForNativeWebSearch(payload);
+    return "native_tool_already_present";
+  }
+
+  payload.tools = [...filteredTools, COPILOT_WEB_SEARCH_TOOL];
+  raiseMinimalReasoningForNativeWebSearch(payload);
+  return "injected";
+}
+
+export function createCopilotNativeWebSearchWrapper(
+  baseStreamFn: StreamFn,
+  params: { config?: OpenClawConfig },
+): StreamFn {
+  return (model, context, options) => {
+    if (!shouldEnableCopilotNativeWebSearch({ config: params.config, model })) {
+      return baseStreamFn(model, context, options);
+    }
+    return streamWithPayloadPatch(baseStreamFn, model, context, options, (payload) => {
+      patchCopilotNativeWebSearchPayload(payload);
+    });
+  };
+}